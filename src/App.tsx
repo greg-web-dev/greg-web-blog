d70325cfaefdf359d4499240116205c092ef4caa
Gregory Katz <gkatz97@gmail.com>
2021-10-29 12:18:38 -0400

    Added web components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Menu, MenuBtn } from '@greg-web-dev/web-components';
+import { useState } from 'react';
+
 const App = () => {
-  return <div>hello</div>;
+  const [active, setActive] = useState(false);
+
+  const menuOptions = [
+    {
+      name: 'Home',
+      url: '#home',
+    },
+    {
+      name: 'sadklfjadlkfj',
+      url: '#blog',
+    },
+  ];
+
+  return (
+    <Router>
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          backgroundColor: 'skyblue',
+        }}
+      >
+        <Menu active={active} onClick={() => setActive(false)} menuOptions={menuOptions} />
+        <MenuBtn
+          active={active}
+          onClick={() => setActive(!active)}
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            paddingRight: 8,
+          }}
+        />
+      </div>
+    </Router>
+  );
 };
 
 export default App;
